350684cf7d318abd349115b7fbc98e7b813e5ffb
lucastoppa <luca.stoppa@codersinternational.com>
2020-09-22 04:01:59 -0700

    [GRAPHS] Added cod for BFS

--- graphs.cpp
+++ graphs.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 namespace graph {
 
@@ -13,6 +14,7 @@ template <typename T>
 struct vertex {
 	bool visited{false};
 	T data;
+	int parent;
 	std::vector<edge> adj;
 };
 
@@ -22,7 +24,7 @@ struct graph {
 	
 	graph(int number_of_vertices) {
 		for (int i=0; i < number_of_vertices; ++i)
-			vertices.push_back({false, i, {}});
+			vertices.push_back({false, i, -1, {}});
 	}
 	
 	void add_edges(int vertex, std::initializer_list<int> edges) {
@@ -40,34 +42,67 @@ struct graph {
 
 	void print() const {
 		for (const auto& v : vertices) {
-			std::cout <<'{' <<v.data <<',' <<v.visited <<",{";
+			std::cout <<'{' <<v.data <<',' <<v.visited <<',' <<v.parent <<',' <<",{";
 			for (const auto& e : v.adj)
 				std::cout <<e.vertex_index <<',';
 			std::cout <<"}}\n";
 		}
 	}
-
-	void revisit(int vertex_index) {
-		if  (vertex_index < vertices.size()) {
-			vertex<T>& v=vertices[vertex_index];
-			if (!v.visited)
-				dfs_visit(v);
+	
+	void reset_data() {
+		for (auto&& v : vertices) {
+			v.visited = false;
+			v.parent = -1;
 		}
 	}
 
-	void dfs_visit(vertex<T>& v) {
-		for (edge& e : v.adj)  {
+	void dfs() {
+		reset_data();
+			
+		for (int i=0; i<vertices.size(); ++i) {	
+			if (!vertices[i].visited)
+				dfs_visit(i);
+		}
+	}
+
+	void dfs_visit(int i) {
+		for (edge& e : vertices[i].adj)  {
 			vertex<T>& curr = vertices[e.vertex_index];
 			if (!curr.visited) {
-				curr.visited = true;
-				dfs_visit(curr);
+				curr.parent = i;
+				dfs_visit(e.vertex_index);
 			}
 		}
+		vertices[i].visited=true;
 	}
 
 	bool reachable(int from, int to) {
-		revisit(from);
-		return vertices[to].visited;
+		bfs(from);
+		
+		vertex<T>& start = vertices[from];
+		vertex<T>& curr = vertices[to];
+		while (curr.parent != -1 && curr.data != start.data)
+			curr = vertices[curr.parent];
+		return curr.data == start.data;
+	}
+
+  void bfs(int from) {
+		reset_data();
+		
+		std::queue<int> q;
+		q.push(from);
+		while (!q.empty()) {
+			int curr_index = q.front(); q.pop();
+			vertex<T>& u = vertices[curr_index];
+			for (const edge& e : u.adj) { 
+				vertex<T>& v = vertices[e.vertex_index];
+				if (!v.visited) {
+					v.parent = curr_index;
+					q.push(e.vertex_index);
+				}
+			}
+			u.visited = true;
+		}
 	}
 };
 
@@ -75,14 +110,17 @@ struct graph {
 
 int main() {
 	graph::graph<int> g{4};
-	g.add_edges(0, {1,2});
-	g.add_edges(1, {1});
+	g.add_edges(0, {1});
+	g.add_edges(1, {2});
 	g.add_edges(2, {});
 	g.add_edges(3, {1,2});
 
 	g.print();
-	g.revisit(3);
-	std::cout <<g.reachable(3, 0) <<'\n';		
+	//g.bfs(0);
+	g.dfs();
+	std::cout <<'\n';
+	//g.revisit(3);
+	//std::cout <<g.reachable(0, 3) <<'\n';		
 	g.print();
 
 	return 0;
